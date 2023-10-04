63160739abf1f58e0d06068e52fd6e08a9ede517
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-04 19:52:23 +0300

    Добавьте файлы проекта.

---
+++ Drob/Drob.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Fraction
+{
+	int numerator; // Числитель
+	int denominator; // Знаменатель
+public:
+	Fraction();
+	~Fraction();
+};
+
+Fraction::Fraction()
+{
+}
+
+Fraction::~Fraction()
+{
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Hello World!\n";
+}
+
